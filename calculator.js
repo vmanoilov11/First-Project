86b7f4e111127583d25739e323ed940e1efdcce2
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-04-11 23:17:56 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function clearResult(){
+    document.getElementById("result").value = ""
+}
+function clickCalculator(value){
+    document.getElementById("result").value += value;
+
+}
+function sum(){
+    let content = document.getElementById("result").value;
+    let result = eval(content);
+    document.getElementById("result").value = result
+
+}
