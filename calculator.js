b13d67ead86faad1aba5f6cfdd26cc44fb19598d
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-05-10 19:12:56 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function clearResult(){
-    document.getElementById("result").value = ""
+    document.getElementById("result").value = "";
 }
 function clickCalculator(value){
     document.getElementById("result").value += value;
@@ -8,6 +8,6 @@ function clickCalculator(value){
 function sum(){
     let content = document.getElementById("result").value;
     let result = eval(content);
-    document.getElementById("result").value = result
+    document.getElementById("result").value = result;
 
 }
